108e9045fdb02505b692177680509b83663ff9fc
Morgan Wahl <morgy.wahl@gmail.com>
2011-09-25 11:16:58 -0400

    update

--- poster.py
+++ poster.py
@@ -10,6 +10,8 @@ import icu
 
 from lxml import etree
 
+SVG_NAMESPACE = 'http://www.w3.org/2000/svg'
+
 scripts = (
     'Goth',
 )
@@ -66,6 +68,18 @@ if __name__ == '__main__':
         help= "output the poster as FILE. defaults to STDOUT.",
         metavar="FILE",
     )
+    parser.add_option(
+        '-w', '--width',
+        dest= 'width',
+        help= "width of the SVG image. defaults to 72in",
+        default= unicode(6 * 12) + "in" # 6 feet
+    )
+    parser.add_option(
+        '-t', '--height',
+        dest= 'height',
+        help= "height of the SVG image. defaults to 36in",
+        default= unicode(3 * 12) + "in" # 3 feet
+    )
     (options, args) = parser.parse_args()
     
     if options.outfile is None:
@@ -73,11 +87,40 @@ if __name__ == '__main__':
     else:
         out = open(options.outfile)
     
-    svg = etree.Element("svg", width="1024", height="500")
+    SVG = "{%s}" % SVG_NAMESPACE
+    NSMAP = {
+        None: SVG_NAMESPACE,
+    }
     
-    for c in get_characters():
-        group = etree.SubElement(svg, "g")
-        text = etree.SubElement(group, "text")
+    svg = etree.Element(SVG + "svg", nsmap=NSMAP, width=options.width, height=options.height, version="1.1")
+    
+    chars = get_characters()
+    # figure out the cell-size, given the poster size, number of cells,
+    # and a 3:4 aspect ratio for the cells.
+    #
+    # cell height: ch
+    # cell width: cw
+    # poster height: ph
+    # poster width: pw
+    # # of rows = r
+    # # of columns = c
+    # # of cells = n
+    # 
+    # cw / ch = 4 / 3
+    # ceil(n / c) = r
+    # r * ch  = ph
+    # c * cw = pw
+    #
+    # cw = (4 * ch) / 3
+    # (4 * c * ch) / 3  = pw
+    # (4 * ch) / 3 = pw / c
+    # 3 / (4 * ch) = c / pw
+    # (3 * pw) / (4 * ch) = c = (3/4) * (pw/ch)
+    # c = (3/4) * (pw/(ph/r)) = (3/4) * ((pw * r)/ph)
+    
+    for c in chars:
+        group = etree.SubElement(svg, SVG + "g")
+        text = etree.SubElement(group, SVG + "text")
         text.text = unicode(c)
     
     out.write(etree.tostring(svg, pretty_print=True, xml_declaration=True, encoding='utf-8'))
